bad10de175e92bc78110efd41987a555657a5fdb
fine2006 <animeshpathak.2006@gmail.com>
2025-03-02 19:44:02 +0530

    add part1 courseinfo till 1.5

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = (props) => {
+  return <h1>{props.name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part[0]} />
+      <Part part={props.part[1]} />
+      <Part part={props.part[2]} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.part[0].exercises +
+        props.part[1].exercises +
+        props.part[2].exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content part={course.parts} />
+      <Total part={course.parts} />
+    </div>
+  );
+};
+
+export default App;
