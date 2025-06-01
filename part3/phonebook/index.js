6429a96790199955c143408e2db37288a047e1cf
fine2006 <animeshpathak.2006@gmail.com>
2025-06-01 12:23:36 +0530

    finish part 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,95 +1,94 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const Contact = require("./models/contact");
-const cors = require("cors");
-const morgan = require("morgan");
-const app = express();
-app.use(express.json());
-app.use(express.static("dist"));
-morgan.token("body", function (req, res) {
-  const body = JSON.stringify(req.body);
-  return body || " ";
-});
+require('dotenv').config()
+const express = require('express')
+const Contact = require('./models/contact')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+app.use(express.json())
+app.use(express.static('dist'))
+morgan.token('body', function (req, res) {
+  const body = JSON.stringify(req.body)
+  return body || ' '
+})
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
-);
-app.use(cors());
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
+app.use(cors())
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
-  next(error);
-};
-app.get("/info", (request, response) => {
-  const date = new Date();
+  next(error)
+}
+app.get('/info', (request, response) => {
+  const date = new Date()
   Contact.find({}).then((contacts) => {
     response.send(
-      `Phonebook has info for ${contacts.length} people <div>${date.toString()}</div>`,
-    );
-  });
-});
-app.get("/api/persons", (request, response) => {
+      `Phonebook has info for ${contacts.length} people <div>${date.toString()}</div>`
+    )
+  })
+})
+app.get('/api/persons', (request, response) => {
   Contact.find({}).then((contacts) => {
-    response.json(contacts);
-  });
-});
+    response.json(contacts)
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Contact.findById(request.params.id)
     .then((contact) => {
       if (contact) {
-        response.json(contact);
+        response.json(contact)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
     .catch((error) => {
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Contact.findByIdAndDelete(request.params.id)
     .then((contact) => response.status(204).end())
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
   const contact = new Contact({
     name: body.name,
     number: body.number,
-  });
+  })
   contact
     .save()
     .then((savedContact) => response.json(savedContact))
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
   Contact.findById(request.params.id)
     .then((contact) => {
       if (!contact) {
-        return response.status(404).end();
+        return response.status(404).end()
       }
 
-      contact.name = name;
-      contact.number = number;
+      contact.name = name
+      contact.number = number
 
       return contact
         .save()
-        .then((updatedContact) => response.json(updatedContact));
+        .then((updatedContact) => response.json(updatedContact))
     })
-    .catch((error) => next(error));
-});
-app.use(errorHandler);
+    .catch((error) => next(error))
+})
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebook/models/contact.js
+++ part3/phonebook/models/contact.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
-const url = process.env.MONGODB_URI;
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB')
   })
   .catch((error) => {
-    "error connecting to MongoDB: ", error.message;
-  });
+    'error connecting to MongoDB: ', error.message
+  })
 
 const contactSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -20,19 +20,19 @@ const contactSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function (v) {
-        return /\d{2,3}-\d{4,}/.test(v);
+        return /\d{2,3}-\d{4,}/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
   },
-});
+})
 
-contactSchema.set("toJSON", {
+contactSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Contact", contactSchema);
+module.exports = mongoose.model('Contact', contactSchema)
